5dc9c967007c868c4f2b7517ca7861bde77518bd
JESLIN <jj20092005@gmail.com>
2025-12-27 14:09:06 +0530

    changes in merging nd deleting files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.182.0/examples/jsm/controls/OrbitControls.js';
 import { createTable, createChair } from './generators.js';
 
 // --- 1. CORE SETUP ---
